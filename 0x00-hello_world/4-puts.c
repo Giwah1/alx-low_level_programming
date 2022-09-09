79688f78e53bc546f53cc2bbf48972e0e9791e29
Giwa Hannah <verahannah2022@gmail.com>
2022-09-08 21:34:21 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *     */
+ *main - Entry point
+ *
+ *Return: By default returns zero.
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
